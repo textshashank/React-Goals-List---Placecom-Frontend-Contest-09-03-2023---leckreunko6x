4e583ad33f0deb9243a7236b95ec5ea695dfc443
textshashank <118591627+textshashank@users.noreply.github.com>
2023-03-09 19:17:47 +0530

    Update GoalForm.js

--- src/components/GoalForm.js
+++ src/components/GoalForm.js
@@ -1,29 +1,41 @@
 import React, { useState } from "react";
 
-const GoalForm = () => {
-    const [formData, setFormData] = useState({
-      goal: "",
-      by: ""
-    });
-  
-    return (
-      <>
-        <h1>My Goals</h1>
-        <form>
-          <input 
-            type="text" 
-            name='goal' 
-            placeholder='Goal...' 
-          />
-          <input 
-            type="text"
-            name="by"
-            placeholder='By...'
-          />
-          <button>Add</button>
-        </form>
-      </>
-    )
-}
+const GoalForm = ({ addGoal }) => {
+  const [formData, setFormData] = useState({
+    goal: "",
+    by: ""
+  });
 
-export default GoalForm;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addGoal(formData);
+    setFormData({ goal: "", by: "" });
+  };
+
+  return (
+    <>
+      <h1>My Goals</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="goal"
+          placeholder="Goal..."
+          value={formData.goal}
+          onChange={(e) =>
+            setFormData({ ...formData, goal: e.target.value })
+          }
+        />
+        <input
+          type="text"
+          name="by"
+          placeholder="By..."
+          value={formData.by}
+          onChange={(e) => setFormData({ ...formData, by: e.target.value })}
+        />
+        <button>Add</button>
+      </form>
+    </>
+  );
+};
+
+export default GoalForm;
